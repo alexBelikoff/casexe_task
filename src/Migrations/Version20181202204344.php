574525852176416a6f66beaa9b28f8f00053bd61
Alexey Belikov <Alexey@beluha.pro>
2018-12-02 23:47:22 +0300

    Added readme & rebuild migration

--- src/Migrations/Version20181202204344.php
+++ src/Migrations/Version20181202204344.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181201200317 extends AbstractMigration
+final class Version20181202204344 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
@@ -23,8 +23,8 @@ final class Version20181201200317 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE prize_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE security.user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE address (id INT NOT NULL, city VARCHAR(64) NOT NULL, steet VARCHAR(128) NOT NULL, house INT NOT NULL, flat INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE lottery (id INT NOT NULL, start_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, end_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, active BOOLEAN DEFAULT NULL, cash_total INT DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE prize (id INT NOT NULL, lottery_id INT NOT NULL, user_id INT DEFAULT NULL, prize_type_id INT NOT NULL, prize_item_id INT DEFAULT NULL, prize_sum INT DEFAULT NULL, prize_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, send_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE lottery (id INT NOT NULL, start_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, end_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, active BOOLEAN DEFAULT NULL, cash_total INT DEFAULT NULL, exchange_coefficient DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE prize (id INT NOT NULL, lottery_id INT NOT NULL, user_id INT DEFAULT NULL, prize_type_id INT NOT NULL, prize_item_id INT DEFAULT NULL, prize_sum INT DEFAULT NULL, prize_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, send_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, convert_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, reject_flag BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_51C88BC1CFAA77DD ON prize (lottery_id)');
         $this->addSql('CREATE INDEX IDX_51C88BC1A76ED395 ON prize (user_id)');
         $this->addSql('CREATE INDEX IDX_51C88BC175D1EEED ON prize (prize_type_id)');
@@ -33,7 +33,7 @@ final class Version20181201200317 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_900F0EB9CFAA77DD ON prize_item (lottery_id)');
         $this->addSql('CREATE TABLE prize_type (id INT NOT NULL, lottery_id INT NOT NULL, name VARCHAR(64) NOT NULL, range_min INT DEFAULT NULL, range_max INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_3CA7C8ECFAA77DD ON prize_type (lottery_id)');
-        $this->addSql('CREATE TABLE security."user" (id INT NOT NULL, address_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, bank_account_num VARCHAR(64) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE security."user" (id INT NOT NULL, address_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, bank_account_num VARCHAR(64) NOT NULL, loyalty_points INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_963093F7F85E0677 ON security."user" (username)');
         $this->addSql('CREATE INDEX IDX_963093F7F5B7AF75 ON security."user" (address_id)');
         $this->addSql('ALTER TABLE prize ADD CONSTRAINT FK_51C88BC1CFAA77DD FOREIGN KEY (lottery_id) REFERENCES lottery (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
